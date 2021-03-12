e378ac917c5718bf8e31754d84f04024763a2f14
Islom Akhmedov <islamuzkg@gmail.com>
2021-03-12 05:58:22 +0000

    Completed this game

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-rock = '''
+import random
+
+while True:
+    rock = '''
     _______
 ---'   ____)
       (_____)
@@ -7,7 +10,7 @@ rock = '''
 ---.__(___)
 '''
 
-paper = '''
+    paper = '''
     _______
 ---'   ____)____
           ______)
@@ -16,7 +19,7 @@ paper = '''
 ---.__________)
 '''
 
-scissors = '''
+    scissors = '''
     _______
 ---'   ____)____
           ______)
@@ -25,9 +28,32 @@ scissors = '''
 ---.__(___)
 '''
 
-choice = int(input("What do you choose? Type 0 for Rock, 1 paper or 2 for Scisor. "))
-my_choice = [rock, paper, scissors]
-computer_choice = [rock, paper, scissors]
-my_choice = my_choice[choice]
-print(f"You choose {my_choice}")
-print(f"Computer chose {computer_choice[2]}")
+    choice = int(input("What do you choose? Type 0 for Rock, 1 paper or 2 for Scisor. "))
+    possible_choices = [rock, paper, scissors]
+    user_choice = possible_choices[choice]
+    random_index = random.randint(0,len(possible_choices)-1)
+    c_choice = possible_choices[random_index]
+
+    print(f"\nYou choose {user_choice} \ncomputer chose {c_choice}.\n")
+
+    if user_choice == c_choice:
+      print("It's a tie!")
+    elif user_choice == rock:
+      if c_choice == scissors:
+        print("Rock smashes scissors! You win!")
+      else:
+        print("Paper covers rock! You lose.")
+    elif user_choice == paper:
+      if c_choice == rock:
+        print("Paper covers Rock! You win!")
+      else:
+        print("Scissors cuts papers! You loose.")
+    elif user_choice == scissors:
+      if c_choice == paper:
+        print("Scissors cut paper! You win!")
+      else:
+        print("Rock smashes scissors! You lose.")
+
+#    play_again = input("Play again? (y/n):")
+#    if play_again.lower() != "y":
+#        break
