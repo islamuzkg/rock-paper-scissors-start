467db0a60841084111f9f114302a7180ac3f3583
Islom Akhmedov <Islamuzkg@gmail.com>
2021-03-13 20:56:16 +0000

    renamed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 import os
 
-while True:
-    rock = '''
+#while True:
+rock = '''
     _______
 ---'   ____)
       (_____)
@@ -11,7 +11,7 @@ while True:
 ---.__(___)
 '''
 
-    paper = '''
+paper = '''
     _______
 ---'   ____)____
           ______)
@@ -20,7 +20,7 @@ while True:
 ---.__________)
 '''
 
-    scissors = '''
+scissors = '''
     _______
 ---'   ____)____
           ______)
@@ -29,34 +29,35 @@ while True:
 ---.__(___)
 '''
 
-    choice = int(input("What do you choose? Type 0 for Rock, 1 paper or 2 for Scisor. "))
-    possible_choices = [rock, paper, scissors]
-    user_choice = possible_choices[choice]
-    random_index = random.randint(0,len(possible_choices)-1)
-    c_choice = possible_choices[random_index]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 paper or 2 for Scisor. "))
+possible_choices = [rock, paper, scissors]
 
-    print(f"\nYou choose {user_choice} \ncomputer chose {c_choice}.\n")
+if user_choice >= 3 or user_choice < 0:
+  print("You entered out of range number, please try again.")
+else:
+  print(possible_choices[user_choice])
 
-    if user_choice == c_choice:
-      print("It's a tie!")
-    elif user_choice == rock:
-      if c_choice == scissors:
-        print("Rock smashes scissors! You win!")
-      else:
-        print("Paper covers rock! You lose.")
-    elif user_choice == paper:
-      if c_choice == rock:
-        print("Paper covers Rock! You win!")
-      else:
-        print("Scissors cuts papers! You loose.")
-    elif user_choice == scissors:
-      if c_choice == paper:
-        print("Scissors cut paper! You win!")
-      else:
-        print("Rock smashes scissors! You lose.")
+c_choice = random.randint(0, 2)
+print("Cmputer chose:\n")
+print(possible_choices[c_choice])
+#[rock, paper, scissors]
+#options = [[draw, lose, win], [win, draw, lose], [lose, win, draw]
+if user_choice == c_choice:
+  print("It is a Draw!")
+elif user_choice == 0 and c_choice == 2:
+  print("You win!")
+elif c_choice == 0 and user_choice == 2:
+  print("You lose!")
+elif c_choice > user_choice:
+  print("You lose")
+elif user_choice > c_choice:
+  print("You win!")
 
-    play_again = input("Play again? (Enter/y/n):")
-    clear = lambda: os.system('clear')
-    clear()
-    if play_again.lower() == "n":
-        break
+
+
+
+    # play_again = input("Play again? (Enter/y/n):")
+    # clear = lambda: os.system('clear')
+    # clear()
+    # if play_again.lower() == "n":
+    #     break
